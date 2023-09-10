a4c9e42fadd8494ee133cd64f649d64e392716b3
Sebastian <sessue-denis@myseneca.ca>
2023-09-10 14:58:08 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name:  Sebastian Denis Student ID: 108534215! Date: ___9/10/2023_____________
+*
+*  Online (Cyclic) URL:  https://blush-anemone-tam.cyclic.app/
+*
+********************************************************************************/ 
+
+
+
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
